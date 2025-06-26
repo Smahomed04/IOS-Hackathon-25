459ce866edf8e7f07ca01b5a9be351cd778c9ea7
TS Rathete <86362482+kgobi16@users.noreply.github.com>
2025-06-26 11:46:36 +1000

    First files

---
+++ IOS-Hackathon-25/IOS-Hackathon-25/IOS_Hackathon_25App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Hackathon_25App.swift
+//  IOS-Hackathon-25
+//
+//  Created by Tlaitirang Rathete on 26/6/2025.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Hackathon_25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IOS-Hackathon-25/IOS-Hackathon-25Tests/IOS_Hackathon_25Tests.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Hackathon_25Tests.swift
+//  IOS-Hackathon-25Tests
+//
+//  Created by Tlaitirang Rathete on 26/6/2025.
+//
+
+import Testing
+@testable import IOS_Hackathon_25
+
+struct IOS_Hackathon_25Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
